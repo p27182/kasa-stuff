36b18e727ebc2fce6fe46da94b7e1abc411f9cb9
babu <p27182@gmail.com>
2025-07-04 12:40:42 -1000

    Add files via upload

---
+++ printer-OFF.bat
@@ -0,0 +1,9 @@
+echo @off
+echo TURNING PRINTER OFF, YOU GUCCI WIDDAT?
+pause
+pause
+kasa --host 10.100.6.47 off --index 0
+kasa --host 10.100.6.47 off --index 1
+kasa --host 10.100.6.47 off --index 2
+echo done
+
---
+++ printer-ON.bat
@@ -0,0 +1,8 @@
+echo @off
+echo TURNING PRINTER ON, YOU GUCCI WIDDAT?
+pause
+kasa --host 10.100.6.47 on --index 0
+kasa --host 10.100.6.47 on --index 1
+kasa --host 10.100.6.47 on --index 2
+echo done
+
